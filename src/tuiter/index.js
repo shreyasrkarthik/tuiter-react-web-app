751c7e6ecd7ff5e6020b3b57af226448e37fcf04
shreyasrkarthik <shreyas.rkarthik@gmail.com>
2022-10-23 15:24:03 -0400

    Completed Navigation Sidebar

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,11 +1,26 @@
 import {Link} from "react-router-dom";
-import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import {Route, Routes} from "react-router";
 
 function Tuiter() {
     return (
-        <div>
-            <Nav/>
-            <h1>Tuiter</h1>
+        <div className="row mt-1">
+            <div className="col-2 col-xl-2 col-lg-2 col-md-2">
+                <NavigationSidebar active="explore" />
+            </div>
+
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+                 style={{ "position": "relative" }}>
+                {/*<Routes>*/}
+                {/*    <Route path="home" element={<HomeComponent />} />*/}
+                {/*    <Route path="explore" element={<ExploreComponent />} />*/}
+                {/*</Routes>*/}
+
+            </div>
+            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                {/*<WhoToFollowList />*/}
+            </div>
+
         </div>
     )
 }
---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {Link, useLocation} from "react-router-dom";
+
+const NavigationSidebar = () => {
+
+
+    const {pathname} = useLocation();
+    const paths = pathname.split('/')
+    const active = paths[2];
+
+    return (
+        <div className="list-group">
+            <Link to={"/"} className={`list-group-item ${active === "Tuiter".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi-twitter wd-icon-width  mt-1"></i> Tuiter
+            </Link>
+            <Link to={"/tuiter/home"} className={`list-group-item ${active === "Home".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi-house-door-fill wd-icon-width  mt-1"></i> Home
+            </Link>
+
+            <Link to={"/tuiter/explore"} className={`list-group-item ${active === "Explore".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi-hash wd-icon-width  mt-1"></i> Explore
+            </Link>
+            <Link to={"/"} className={`list-group-item ${active === "Notifications".toLowerCase() ? "active" : ""}`}>
+                <i className="bi bi-bell-fill wd-icon-width  mt-1"></i> Notifications
+            </Link>
+            <Link to={"/"} className={`list-group-item ${active === "Messages".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi-envelope-fill wd-icon-width  mt-1"></i> Messages
+            </Link>
+            <Link to={"/"} className={`list-group-item ${active === "Bookmarks".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi bi-bookmark-fill wd-icon-width  mt-1"></i> Bookmarks
+            </Link>
+            <Link to={"/"} className={`list-group-item ${active === "Lists".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi-list-ul wd-icon-width  mt-1"></i> Lists
+            </Link>
+            <Link to={"/"} className={`list-group-item ${active === "Profile".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi-person-fill wd-icon-width  mt-1"></i> Profile
+            </Link>
+
+            <Link to={"/"} className={`list-group-item ${active === "More".toLowerCase() ? "active" : ""}`} >
+                <i className="bi bi-three-dots wd-icon-width  mt-1"></i> More
+            </Link>
+        </div>
+    );
+};
+export default NavigationSidebar;
