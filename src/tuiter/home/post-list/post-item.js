868869fe20ba9d483722a953c5374e6af31ab79e
shreyasrkarthik <shreyas.rkarthik@gmail.com>
2022-10-23 18:13:48 -0400

    Fixed Alignment issues

--- src/tuiter/home/post-list/post-item.js
+++ src/tuiter/home/post-list/post-item.js
@@ -1,7 +1,8 @@
 const PostItem = ({ post }) => {
     return (
         <li className="list-group-item p-3 bg-transparent">
-            <img src={post.profileImage} style={{ height: '45px' }} className="img-fluid img-size rounded-circle" alt="avatar" />
+            <img src={post.profileImage} style={{ height: '45px'}} className="img-fluid img-size rounded-circle"
+                 alt="porfile-pic"/>
             <div className="col col-xl-9 ps-2 mb-2 float-start" >
                 <strong>{post.userName}</strong>
                 <span><i className="fa-solid fa-circle-check"></i></span>
@@ -9,25 +10,19 @@ const PostItem = ({ post }) => {
                     @{post.handle} - {post.time}
                 </span>
                 <br />
-                {post.description}
             </div>
             <i className="fa-solid fa-ellipsis text-secondary mx-4" ></i>
             <div className="card col border-secondary wd-mx-5 wd-clear bg-transparent ">
-                <img src={post.descriptionImage} className={
-                    post.topic !== "" && post.description !== "" && post.link !== ""
-                        ? "card-img-top"
-                        : "card-img-top rounded-bottom"
-
-                }
+                <img src={post.descriptionImage} className="card-img-top"
                      alt="post" />
-                {post.topic !== "" && post.description !== "" && post.link !== "" ?
-                    <div className="card-body">
-                        <p className="card-title fs-6 fw-bolder" >{post.topic}</p>
-                        <p className="card-text text-secondary">{post.description}</p>
-                        <a href={post.link} className="text-secondary" style={{ textDecoration: "none" }}>
-                            {post.linkText}
-                        </a>
-                    </div> : <></>}
+
+                <div className="card-body">
+                    <p className="card-title fs-6 fw-bolder" >{post.topic}</p>
+                    <p className="card-text text-secondary">{post.description}</p>
+                    <a href={post.link} className="text-secondary" style={{ textDecoration: "none" }}>
+                        {post.linkText}
+                    </a>
+                </div>
             </div>
             <ul className="nav nav-fill mt-2 text-secondary">
                 <li className="nav-item">
